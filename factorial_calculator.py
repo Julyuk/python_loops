19f5c487a22045e91a896707a55147a447fafd1a
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-03-06 13:01:31 +0200

    Implemented the factorial counter without recursion

---
+++ factorial_calculator.py
@@ -0,0 +1,8 @@
+def factorial_calculator(num):
+    if type(num)==int and num >=0:
+        factorial=1
+        if num !=0 :
+            for i in range(1,num+1):
+                factorial*=i
+        print(factorial)
+        return factorial
